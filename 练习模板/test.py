83845f105b0e065abfa35fbf7be247ee80e5bbf4
Xiaosen9807 <85266718+Xiaosen9807@users.noreply.github.com>
2021-06-13 09:47:19 +0800

    Delete 练习模板 directory

--- 练习模板/test.py
+++
@@ -1,9 +0,0 @@
-#Programmer: Qin Xusen
-#Time: 2021/4/28-22:58
-#%%
-import pandas as pd
-table=pd.read_excel('C:\\Users\\Surface\\Desktop\\123.csv')
-table_df=table.values
-print(table,type(table))
-#table
-                          #%%
--- 练习模板/分词.py
+++
@@ -1,6 +0,0 @@
-#Programmer: Qin Xusen
-#Time: 2021/4/28-17:06
-import jieba
-##l="大道之行也天下为公"
-S=jieba.lcut(l)
-print(S)
